3a1e573aa0e0c66ec8485f4891889a85c8c679c6
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-04-01 17:04:39 +0200

    add footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "../globals.css";
 import Navbar from "@/components/Navbar";
 import ToasterProvider from "@/lib/providers/ToasterProvider";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ClerkProvider>
           <ToasterProvider />
           <Navbar />
           {children}
+          <Footer />
         </ClerkProvider>
       </body>
     </html>
